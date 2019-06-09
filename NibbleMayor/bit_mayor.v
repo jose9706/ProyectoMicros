f20305d8a89a48adf8f3d7c14f379270f27ddb36
Stuart Leal <stuart.leal23@gmail.com>
2019-06-08 21:09:11 -0700

    Lógica negada para el reset

--- NibbleMayor/bit_mayor.v
+++ NibbleMayor/bit_mayor.v
@@ -1,6 +1,6 @@
 module bit_mayor (
                   input      clk,
-                  input      reset,
+                  input      reset_L,
                   input      bm_a,
                   input      bm_b,
                   output reg bm_selector,
@@ -8,10 +8,7 @@ module bit_mayor (
                   );
 
    always @ (posedge clk) begin
-      if (reset) begin
-         bm_selector <= 0;
-         bm_distintos <= 0;
-      end else begin
+     if (reset_L) begin
          case ({bm_a, bm_b})
            2'b00 : begin
               bm_selector <= bm_selector;
@@ -30,6 +27,9 @@ module bit_mayor (
               bm_distintos <= 0;
            end
          endcase // case ({bm_a, bm_b})
+      end else begin       
+         bm_selector <= 0;
+         bm_distintos <= 0;
       end // end else if reset
    end // end always posedge clk
 
