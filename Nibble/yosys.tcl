1f7400f2306f0f04550b66e9100d5eea70935568
giancamarin <gianca.1996mh@gmail.com>
2019-06-27 21:39:10 -0600

    Adicion de sintesis flat en el makefile y .tcl

--- Nibble/yosys.tcl
+++ Nibble/yosys.tcl
@@ -13,13 +13,21 @@ yosys opt
 #mapping to internal cell library
 yosys techmap
 yosys opt
+#flatten synthesis
+yosys flatten
+yosys opt
+# cleaning optimization
+yosys opt_clean -purge
 # mapping flip-flops to mycells.lib
 yosys dfflibmap -liberty $::env(LIB)
 #mapping logic to mycells.lib
 yosys abc -liberty $::env(LIB)
+yosys opt
 #statistics
 yosys stat -liberty $::env(LIB)
 #clean up
 yosys clean
+#show gates
+#yosys show -stretch -prefix $::env(TOP_MODULE) -format dot ./dot/$::env(OUTPUT_SYNTH)
 #write synthetized design
-yosys write_verilog $::env(OUTPUT_SYNTH)
+yosys write_verilog -attr2comment $::env(OUTPUT_SYNTH)
